5ae06d6a7f21e339fd76d6cdcb0590c7e4cf94d4
geo <geovannigalan3@gmail.com>
2022-12-24 17:38:25 -0800

    got loop. need to get runing in game function

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ computerScore = 0;
 
    /* display console player choice */
    
-const playerSelection =  prompt("rock paper or scissors?").toLowerCase();
-const computerSelection = getComputerChoice(items)
+
+
     
 
-console.log(playRound(playerSelection, computerSelection));
+
 
 // PlayRound function (1 game)
 function playRound (playerSelection, computerSelection) {
@@ -22,14 +22,14 @@ function playRound (playerSelection, computerSelection) {
        (playerSelection == "paper" && computerSelection == "rock"))
         { 
         playerScore += 1;
-        return `you win! ${playerSelection} beats ${computerSelection}!! \n ${playerScore} `;
+        return `you win! ${playerSelection} beats ${computerSelection}!! \n\n Player points: ${playerScore} \n\n computer points: ${computerScore}`;
         } 
     else if ((playerSelection == "scissors" && computerSelection == "rock") ||
         (playerSelection == "rock" && computerSelection == "paper") ||
         (playerSelection == "paper" && computerSelection == "scissors")) 
         {
         computerScore += 1;
-        return `you lose! ${computerSelection} beats ${playerSelection}!!`    
+        return `you lose! ${computerSelection} beats ${playerSelection}!! \n\n Computer points: ${computerScore} \n\n player score: ${playerScore}`;    
         }
     else if (playerSelection == computerSelection) {
         return "its a tie! play again!";
@@ -39,7 +39,17 @@ function playRound (playerSelection, computerSelection) {
     }
 }
 
+function game() {
 
+    for (let i = 0; i<5; i++) {
+        const playerSelection =  prompt("rock paper or scissors?").toLowerCase();
+        const computerSelection = getComputerChoice(items)
+        console.log(playRound(playerSelection, computerSelection));    
+    }
+    return (playerScore > computerScore) ? "you won!": "You lost!";
+}
+
+console.log(game);
 /* looped game. game function in here looped */
 
 
