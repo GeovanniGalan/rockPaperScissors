954ca9812ec02f22d4aa9b89bf579bbbbc8167dc
geo <geovannigalan3@gmail.com>
2022-12-24 17:16:24 -0800

    new design playRound function points

--- index.js
+++ index.js
@@ -1,63 +1,41 @@
-
-
 const items = ["rock", "paper", "scissors"];
 
 function getComputerChoice(items) {
     return  items[Math.floor(Math.random() * items.length)];
 }
+playerScore = 0;
+computerScore = 0;
 
 
+   /* display console player choice */
+   
+const playerSelection =  prompt("rock paper or scissors?").toLowerCase();
+const computerSelection = getComputerChoice(items)
+    
 
-
-
+console.log(playRound(playerSelection, computerSelection));
 
 // PlayRound function (1 game)
 function playRound (playerSelection, computerSelection) {
-    if (playerSelection == "rock") {
-
-        if (computerSelection == "paper") {
-            return ("You lose! Paper beats rock");
+    if ((playerSelection == "rock" && computerSelection == "scissors") ||
+       (playerSelection == "scissors" && computerSelection == "paper") ||
+       (playerSelection == "paper" && computerSelection == "rock"))
+        { 
+        playerScore += 1;
+        return `you win! ${playerSelection} beats ${computerSelection}!! \n ${playerScore} `;
+        } 
+    else if ((playerSelection == "scissors" && computerSelection == "rock") ||
+        (playerSelection == "rock" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "scissors")) 
+        {
+        computerScore += 1;
+        return `you lose! ${computerSelection} beats ${playerSelection}!!`    
         }
-        else if (computerSelection == "scissors") {
-            return ("you win! rock smashes scissors!");
-        }
-        else if (computerSelection == "rock") {
-            return ("you tied! try again.");
-        }
-        else {
-            return (undefined);
-        }
+    else if (playerSelection == computerSelection) {
+        return "its a tie! play again!";
     }
-
-    else if (playerSelection == "paper") {
-
-        if (computerSelection == "rock") {
-            return ("you win! paper wraps rock!");
-        }
-        else if (computerSelection == "paper") {
-           return ("you tied! try again.");
-        }
-        else if (computerSelection == "scissors") {
-            return ("you lose! scissors cut paper!");
-        }
-        else {
-            return (undefined);
-        }
-    }
-    else if (playerSelection == "scissors") {
-
-        if (computerSelection == "rock") {
-            return ("you lose! rock smashes scissors.");
-        }
-        else if (computerSelection == "paper") {
-            return ("you win! scissors slice paper!")
-        }
-        else if (computerSelection == "scissors") {
-            return ("you tied, try again.");
-        }
-        else {
-            return (undefined);
-        }
+    else {
+        return undefined;
     }
 }
 
@@ -65,16 +43,10 @@ function playRound (playerSelection, computerSelection) {
 /* looped game. game function in here looped */
 
 
-  for (let i = 0; i < 5; i++) {
-   /* display console player choice */
-    playerCard = 0;
-    computerCard = 0;
-    const playerSelection =  prompt("rock paper or scissors?").toLowerCase();
-    const computerSelection = getComputerChoice(items)
-    console.log(playRound(playerSelection, computerSelection));
+  
+
     
-    
-  }  
+  
     
 
 
