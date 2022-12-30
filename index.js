de1619c392aaaa2c1274dbfedb4a6eca097871a8
geo <geovannigalan3@gmail.com>
2022-12-29 21:41:51 -0800

    condensing space code took

--- index.js
+++ index.js
@@ -5,15 +5,11 @@ const items = ["rock", "paper", "scissors"];
 function getComputerChoice(items) {
     return  items[Math.floor(Math.random() * items.length)];
 }
-
 //scores  
 playerScore = 0;
 computerScore = 0;
-
-
 // PlayRound function (1 game)
 function playRound (playerSelection, computerSelection) {
-
     if ((playerSelection == "rock" && computerSelection == "scissors") ||
        (playerSelection == "scissors" && computerSelection == "paper") ||
        (playerSelection == "paper" && computerSelection == "rock")) 
@@ -30,22 +26,15 @@ function playRound (playerSelection, computerSelection) {
         computerScore += 1;
         return `you lose! ${computerSelection} beats ${playerSelection}!! \n\n Computer points: ${computerScore} \n\n player score: ${playerScore}`;    
         }
-      
-    
-
     else if (playerSelection == computerSelection) {
         return `its a tie! \n\n player points:  ${playerScore} \n\n Computer Score: ${computerScore}`;
     }
-
-
     else {
         return undefined;
     }
 }   
-
-//5 round game, with working for loop
+//5 round game, with working for-loop. anounces winner after loop
 function game(result) {   
-     
     for (let i = 0; i < 5; i++) {
         const playerSelection =  prompt("rock paper or scissors?").toLowerCase();
         const computerSelection = getComputerChoice(items)
@@ -60,8 +49,8 @@ function game(result) {
     else {
         console.log("you tied with the computer. try again.");
     }
-   
 }
-
 //running 5 round game(rock paper scissors) in this function
 game();
+
+const btn btn1 = document.createElement('#btn')
