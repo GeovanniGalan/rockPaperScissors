c7f4e992ff8c984ca9845566246d073613a96bd3
geo <geovannigalan3@gmail.com>
2022-12-29 22:50:34 -0800

    retrying from the start DOM

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const items = ["rock", "paper", "scissors"];
 function getComputerChoice(items) {
     return  items[Math.floor(Math.random() * items.length)];
 }
+
 //scores  
 playerScore = 0;
 computerScore = 0;
+
+
 // PlayRound function (1 game)
 function playRound (playerSelection, computerSelection) {
+
     if ((playerSelection == "rock" && computerSelection == "scissors") ||
        (playerSelection == "scissors" && computerSelection == "paper") ||
        (playerSelection == "paper" && computerSelection == "rock")) 
@@ -26,52 +30,38 @@ function playRound (playerSelection, computerSelection) {
         computerScore += 1;
         return `you lose! ${computerSelection} beats ${playerSelection}!! \n\n Computer points: ${computerScore} \n\n player score: ${playerScore}`;    
         }
+      
+    
+
     else if (playerSelection == computerSelection) {
         return `its a tie! \n\n player points:  ${playerScore} \n\n Computer Score: ${computerScore}`;
     }
+
+
     else {
         return undefined;
     }
 }   
-//5 round game, with working for-loop. anounces winner after loop
+
+//5 round game, with working for loop
 function game(result) {   
-
-
-        for (let i = 0; i < 5; i++) {
-            const playerSelection =  prompt("rock paper or scissors?");
-            if (playerSelection === null) {
-                console.log("too scared? load screen to test your luck.");
-                break;
-                
-            }
-            else {
-                playerSelection.toLowerCase(); 
-            }
-        }
-
-
-            const computerSelection = getComputerChoice(items)
-            result = playRound(playerSelection, computerSelection);
-            return result;  
-    
-        }
-        
-        if (playerScore > computerScore) {
-            console.log("you win! reload to play again!");
-        }
-        else if (computerScore > playerScore) {
-            console.log("computer wins! reload to play again!");
-        }
-
-        else if (computerScore === playerScore) {
-            console.log("you tied with the computer. try again.");
-        }
-
-        else {
-            console.log('game over')
-        }
-        
+     
+    for (let i = 0; i < 5; i++) {
+        const playerSelection =  prompt("rock paper or scissors?").toLowerCase();
+        const computerSelection = getComputerChoice(items)
+        result = console.log(playRound(playerSelection, computerSelection));  
+    }
+    if (playerScore > computerScore) {
+        console.log("you win! reload to play again!");
+    }
+    else if (computerScore > playerScore) {
+        console.log("computer wins! reload to play again!");
+    }
+    else {
+        console.log("you tied with the computer. try again.");
+    }
+   
+}
 
 //running 5 round game(rock paper scissors) in this function
 game();
-
